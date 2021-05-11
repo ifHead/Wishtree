3d82535b7b9bd770c3fbf8ce0cef59322af7ad03
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-05-11 19:22:43 +0900

    working on --

--- js/p5.js/wish_tree.js
+++ js/p5.js/wish_tree.js
@@ -1,73 +1,16 @@
-let x = 0; // x coordiante for lines
-let y = 0; // y coordinate for lines
-
-let stepRight = 0; // x coordinate takes a step to the right.
-let direction = 1; // reverses the direction of y growth.
-let rotateMatrix = 0; // value for rotate()
-let translateX, translateY = 0; // value for translate()
-let loopCount = 1; // determines traslation and rotation
-let growth; // it determines growth range.
-let p = 0.5;
-
-let displaySize = 500;
-let drawloopcount = 0;
+let customHeight = 600;
 
 function setup() {
-  createCanvas(windowWidth, displaySize);
-  background(0);
-  growth = random(20, 50);
+  const thisCanvas = createCanvas(windowWidth, customHeight);
+  thisCanvas.parent('wishtreep5Div');
 }
 
 function windowResized(){
-  resizeCanvas(windowWidth, displaySize);
+  resizeCanvas(windowWidth, customHeight);
 }
 
 function draw() {
-  noStroke();
-  if(drawloopcount%2 == 0) {
-    fill(0, 2);
-    rect(0, 0, width, height);
-  } else if (drawloopcount%3 == 0) {
-    fill(0, random(5,40));
-    rect(0, 0, width, height);
-  }
-  stroke(255);
-  strokeWeight(2);
-
-  translate(translateX,translateY);
-  
-  if (random() > p) {
-    rotate(radians(rotateMatrix));
-  	line(x, y, x+growth, y+(direction*growth)); // draw backslash
-  } else {
-    rotate(radians(rotateMatrix));
-  	line(x+growth, y, x, y+(direction*growth)); // draw slash
-  }
-  
-  if (y > height || y < 0) {
-    stepRight += growth;
-    direction = direction * -1;
-  }
-
-  if (x >= 0) {
-    y += direction*growth;
-    x = stepRight;
-  }
-  
-  if (x > width){
-    x = 0;
-    y = 0;
-    stepRight = 0;
-    direction = 1;
-    growth = random(30,100);
-    
-    loopCount = 1;
-    
-
-    translateX = 0;
-    translateY = 0;
-    rotateMatrix = 0;
-  }
-  
-  drawloopcount ++;
+  clear();
+  fill(0);
+  ellipse(width/2, customHeight/2, 50, 50);
 }
