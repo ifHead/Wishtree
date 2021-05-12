e78bd2ea903708da8d422a2752e1986d8f464038
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-05-12 11:17:21 +0900

    p5.js sketch flexbox and easing, smooth scroll, parallax interaction

--- js/p5.js/wish_tree.js
+++ js/p5.js/wish_tree.js
@@ -1,16 +1,30 @@
-let customHeight = 600;
-
 function setup() {
-  const thisCanvas = createCanvas(windowWidth, customHeight);
+  const thisCanvas = createCanvas(windowWidth, windowHeight);
   thisCanvas.parent('wishtreep5Div');
 }
 
 function windowResized(){
-  resizeCanvas(windowWidth, customHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
+let x = 0;
+let y = 0;
+let dx = 0;
+let dy = 0;
+let easing = 0.05;
+let targetX = 0;
+let targetY = 0;
+
 function draw() {
-  clear();
+  clear(0);
+  targetX = mouseX;
+  dx = targetX - x;
+  x += dx * easing;
+  targetY = mouseY;
+  dy = targetY - y;
+  y += dy * easing;
+  
   fill(0);
-  ellipse(width/2, customHeight/2, 50, 50);
+  textSize(100);
+  ellipse(x, y, 50, 50);
 }
