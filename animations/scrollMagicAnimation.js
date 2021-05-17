f921cc0807036aa1dfd735d4a8fc290b2dd9ba3d
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-05-17 20:16:43 +0900

    multiple changes
    smooth scroll effect + navigator
    Change color background (need to decide: change at once or change depending on the amount of scroll)
    alt error image
    p5js load error noise

--- animations/scrollMagicAnimation.js
+++ animations/scrollMagicAnimation.js
@@ -2,7 +2,7 @@ const flightPath = {
   curviness: 1.5,
   autoRotate: true,
   values: [
-    {x: 100, y: -20},
+    {x: 0, y: -300},
     {x: 300, y: 10}, 
     {x: 500, y: 100}, 
     {x: 750, y: -100}, 
@@ -13,19 +13,29 @@ const flightPath = {
   ]
 }
 
-const tween = new TimelineLite();
-
-tween.add(
-  TweenLite.to('.paper-plane', 1, {
-    bezier: flightPath,
-    ease: Power1.easeInOut
-  })
-);
-
 const controller = new ScrollMagic.Controller();
+var body_elem = document.querySelector("body");
+const timeline = new TimelineLite();
 
-const scene = new ScrollMagic.Scene({ 
-	triggerElement: ".animation",
-	duration: 1500,
-	triggerHook: 0.2
-}).setTween(tween).addIndicators().setPin('.animation').addTo(controller);
+timeline.add(
+  TweenLite.to('#verytoplogo', 2, {
+    opacity: 1
+  })
+)
+const reveal_verytoplogo_scrollmagic = new ScrollMagic.Scene({
+  triggerElement: "#trigger_reveal_verytoplogo",
+  duration: 100,
+  triggerHook: 0.2
+}).setTween(reveal_verytoplogo_timeline).addIndicators().addTo(controller);
+
+
+timeline.add(
+  TweenLite.to('#verytoplogo', 2, {
+    opacity: 0
+  })
+)
+const hide_verytoplogo_scrollmagic = new ScrollMagic.Scene({
+  triggerElement: "#trigger_hide_verytoplogo",
+  duration: 100,
+  triggerHook: 0.2
+}).setTween(hide_verytoplogo_timeline).addIndicators().addTo(controller);
