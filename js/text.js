c8f13b1e154cbfbaddd2687441c75a3ec26c46a2
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-06-07 11:47:41 +0900

    firebase works (not in p5) / video not playing bug fixed

--- js/text.js
+++ js/text.js
@@ -1,10 +1,12 @@
 var isTextExist = false;
+var currentWishText = "";
 
 function inputButton(){
 	if(document.getElementById('wishtext').value == ""){
 		alert('입력되지 않았어요!');
 	} else {
 		alert('소망을 걸었어요!');
+		currentWishText = document.getElementById('wishtext').value;
 		isTextExist = true;
 	}
 }
