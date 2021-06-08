672c26a9c0c2dace82bbbcdea967e37d3a839d3e
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-06-08 10:10:26 +0900

    big div tag

--- js/p5.js/ink.js
+++ js/p5.js/ink.js
@@ -1,4 +1,3 @@
-
 const cosX = new Float32Array([
   0.500,  0.492,  0.470,  0.433,  0.383,  
   0.321,  0.250,  0.171,  0.087,  0.000,  
@@ -20,9 +19,16 @@ const sinY = new Float32Array([
   -0.087]);
 
 let ink = new Array(300);
+let init = 0;
 
 function get_currentWishText(){
   console.log(currentWishText);
+}
+
+let mySound;
+function preload(){
+  soundFormats('ogg');
+  mySound = loadSound('../../assets/le_festin.ogg');
 }
 
 function setup() {
@@ -32,68 +38,9 @@ function setup() {
     for(let i = 0; i < 300; i++){
       ink[i] = new Ink(200,200);
     }    
+    
+    mySound.play();
 }
-
-class Ink{
-    constructor(_x, _y){
-        this.ten_track = new Int8Array(10);
-        this.x = _x;
-        this.y = _y;
-        this.alpha = 10;
-        this.ink_amount = 5;
-        this.choice = floor(random(0,37));
-        for(let i = 0; i < 10; i++){
-            this.ten_track[i] = this.choice + i;
-            if(this.ten_track[i] > 35) { this.ten_track[i] += -36; }
-        }
-        
-        this.active_crossroad = 0;
-        this.seq_crossroad = {
-          first : random(10, 20),
-          second : random(35, 45),
-          third : random(60, 70)
-        }
-    }
-    drop(){
-        this.proceed = this.ten_track[floor(random(0,10))];
-        let r = random(-0.3,0.3);
-        let r2 = random(-0.3,0.3);
-        this.x += cosX[this.proceed] + r;
-        this.y += sinY[this.proceed] + r2;
-        this.alpha = lerp(this.alpha, 0, 0.03);
-        this.ink_amount = lerp(this.ink_amount, 0, 0.03);
-        strokeWeight(this.ink_amount);
-        stroke(0, this.alpha);
-        point(this.x,this.y);
-    }
-    crossroad(){
-      this.init_crossroad();
-      
-      if(this.active_crossroad < 3){
-        
-      }
-      if(this.active_crossroad < 3){
-        
-      }
-      if(this.active_crossroad == 3) {
-        
-      }
-    }
-  
-    init_crossroad(){
-      if(this.seq_crossroad.third < frameCount()){
-        this.active_crossroad = 3;
-      }
-      else if(this.seq_crossroad.second < frameCount()){
-        this.active_crossroad = 2;
-      }
-      else if(this.seq_crossroad.first < frameCount()){
-        this.active_crossroad = 1;
-      }
-    }
-}
-
-let init = 0;
 
 function draw() {
     for(let i = 0; i < 50; i++){
@@ -104,4 +51,76 @@ function draw() {
     if(frameCount % 10 == 0){
       get_currentWishText();
     }
+    
+    var scrollValue = $(document).scrollTop();
+    if(scrollValue > 1000 && scrollValue < 5000){
+      mySound.setVolume(map(scrollValue, 1000, 5000, 0, 1));
+    } 
+    else if(scrollValue > 5000 && scrollValue < 9000) {
+      mySound.setVolume(map(scrollValue, 5000, 9000, 1, 0));
+    }
+    else {
+      mySound.setVolume(0);
+    }
+    console.log(mySound.volume);
+}
+
+
+class Ink{
+  constructor(_x, _y){
+      this.ten_track = new Int8Array(10);
+      this.x = _x;
+      this.y = _y;
+      this.alpha = 10;
+      this.ink_amount = 5;
+      this.choice = floor(random(0,37));
+      for(let i = 0; i < 10; i++){
+          this.ten_track[i] = this.choice + i;
+          if(this.ten_track[i] > 35) { this.ten_track[i] += -36; }
+      }
+      
+      this.active_crossroad = 0;
+      this.seq_crossroad = {
+        first : random(10, 20),
+        second : random(35, 45),
+        third : random(60, 70)
+      }
+  }
+  drop(){
+      this.proceed = this.ten_track[floor(random(0,10))];
+      let r = random(-0.3,0.3);
+      let r2 = random(-0.3,0.3);
+      this.x += cosX[this.proceed] + r;
+      this.y += sinY[this.proceed] + r2;
+      this.alpha = lerp(this.alpha, 0, 0.03);
+      this.ink_amount = lerp(this.ink_amount, 0, 0.03);
+      strokeWeight(this.ink_amount);
+      stroke(0, this.alpha);
+      point(this.x,this.y);
+  }
+  crossroad(){
+    this.init_crossroad();
+    
+    if(this.active_crossroad < 3){
+      
+    }
+    if(this.active_crossroad < 3){
+      
+    }
+    if(this.active_crossroad == 3) {
+      
+    }
+  }
+
+  init_crossroad(){
+    if(this.seq_crossroad.third < frameCount()){
+      this.active_crossroad = 3;
+    }
+    else if(this.seq_crossroad.second < frameCount()){
+      this.active_crossroad = 2;
+    }
+    else if(this.seq_crossroad.first < frameCount()){
+      this.active_crossroad = 1;
+    }
+  }
 }
