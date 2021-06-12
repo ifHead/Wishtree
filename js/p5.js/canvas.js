677d44d0694ce4512a9322592557d2eaab24ba9e
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-06-13 00:52:05 +0900

    .

--- js/p5.js/canvas.js
+++ js/p5.js/canvas.js
@@ -39,14 +39,13 @@ let ink_sketch = function(p){ // 잉크 스케치
   p.draw = function() {
     p.cnv.position(window.innerWidth/2 - 250,1150);
     p.receivedText = currentWishText;
-    console.log(p.receivedText);
     p.textAlign(p.CENTER, p.CENTER);
     p.textSize(32);
     p.fill(0);
     p.noStroke();
     p.text(p.receivedText, 120, 150);
     for(let i = 0; i < 50; i++){
-      p.ink[i].drop();  
+      p.ink[i].drop();
     }
     p.stroke(0);
     p.noFill();
@@ -99,20 +98,23 @@ let test_sketch = function(p){
   }
 }
 
-let sound_sketch = function(p){
+let sound_sketch_N_blockingGUI = function(p){
   var mySound;
   p.preload = function(){
     p.soundFormats('ogg');
     mySound = p.loadSound('../../assets/le_festin.ogg')
   }
   p.setup = function() {
-    p.cnv = p.createCanvas(500, 500);
-    p.cnv.position(90, 90);
+    // p.elem = createElement
+    p.cnv = p.createCanvas(window.innerWidth, window.innerHeight);
+    p.cnv.parent('fullpageCanvas');
+    p.cnv.position(0,0);
     mySound.setVolume(0);
     mySound.play();
+    //-------------------------sound ready
   }
   p.draw = function() {
-    // p.rect(0,0,500,500);
+    p.clear();
     var scrollValue = $(document).scrollTop();
     if(scrollValue > 1000 && scrollValue < 5000){
       mySound.setVolume(p.map(scrollValue, 1000, 5000, 0, 1));
@@ -123,10 +125,157 @@ let sound_sketch = function(p){
     else {
       mySound.setVolume(0);
     }
-  } 
+
+    // p.modaler = p.constrain(p.map(p.modal_w, 0, 100, 0, 28), 0, 28);
+    // p.modal_h = p.lerp(p.modal_h, 0, 180, 0.03);
+    // p.modal_w = p.lerp(p.modal_w, 0, 350, 0.03);
+    p.drawingContext.shadowOffsetX = 2;
+    p.drawingContext.shadowOffsetY = 2;
+    p.drawingContext.shadowBlur = 24;
+    p.drawingContext.shadowColor = p.color(0,0,0,p.modal_alpha/2);
+    p.clear();
+    p.background(0, p.bgalpha);
+    p.noStroke();
+
+    p.fill(255, p.modal_alpha);
+    p.rect(window.innerWidth/2-175, window.innerHeight/2-90, p.modal_w, p.modal_h, 10,10,10,10);
+    p.togglebool = modal_state;
+    
+    p.stroke(0,255);
+    p.strokeWeight(5);
+    p.fill(0);
+    p.textSize(20);
+    p.text(window.innerWidth/2, window.innerHeight/2, "asdfasdf소망을 입력해주셔야 해요!");
+
+    if(p.togglebool == true){
+        p.modal_w_target = 350;
+        p.d_modal_w = p.modal_w_target - p.modal_w;
+        p.modal_w += p.d_modal_w * p.easing;
+        p.bgalpha_target = 100;
+        p.d_bgalpha = p.bgalpha_target - p.bgalpha;
+        p.bgalpha += p.d_bgalpha * 0.03;
+        
+        p.modal_alpha_target = 255;
+        p.d_modal_alpha = p.modal_alpha_target - p.modal_alpha;
+        p.modal_alpha += p.d_modal_alpha * 0.35;
+      if(p.modal_w > 340){
+        p.modal_h_target = 220;
+        p.d_modal_h = p.modal_h_target - p.modal_h;
+        p.modal_h += p.d_modal_h * p.easing;
+
+        if(p.modal_h > 200) {
+          p.closebuttonAlpha = p.lerp(p.closebuttonAlpha, 255, 0.05);
+        }
+      }  
+      p.button();
+    }
+
+    if(p.togglebool == false){
+      p.bgalpha_target = 0;
+      p.d_bgalpha = p.bgalpha_target - p.bgalpha;
+      p.bgalpha += p.d_bgalpha * 0.03;
+
+      p.modal_h_target = 20;
+      p.d_modal_h = p.modal_h_target - p.modal_h;
+      p.modal_h += p.d_modal_h * p.easing;
+      if(p.modal_h < 23){
+        p.modal_w_target = 20;
+        p.d_modal_w = p.modal_w_target - p.modal_w;
+        p.modal_w += p.d_modal_w * p.easing;
+
+        p.modal_alpha_target = 0;
+        p.d_modal_alpha = p.modal_alpha_target - p.modal_alpha;
+        p.modal_alpha += p.d_modal_alpha * 0.05;
+
+        p.closebuttonToggle *= -1;
+      }
+      p.closebuttonAlpha = p.lerp(p.closebuttonAlpha, 0, 0.27);
+      
+      p.button();
+    }
+
+    p.button_hover();
+  }
+
+  p.button = function(){
+      p.drawingContext.shadowOffsetX = 0;
+      p.drawingContext.shadowOffsetY = 0;
+      p.drawingContext.shadowBlur = 5;
+      p.drawingContext.shadowColor = p.color(0,0);
+      if(p.togglebool == true){
+        
+      } else {
+        
+      }
+      p.fill(p.button_color);
+      p.stroke(p.button_color);
+      if(p.hover_bool == true){ 
+        p.close_button_border_thickness = p.lerp(p.close_button_border_thickness, 4, 0.15);
+      } else {
+        p.close_button_border_thickness = p.lerp(p.close_button_border_thickness, 0, 0.15);
+      }
+      p.strokeWeight(p.close_button_border_thickness);
+      p.rect(window.innerWidth/2+134, window.innerHeight/2-77, 30, 30, 7,7,7,7);
+      p.drawX(window.innerWidth/2+143, window.innerHeight/2-68, 12);
+  }
+
+  p.drawX = function(x, y, size){
+      p.stroke(255, p.closebuttonAlpha);
+      p.strokeWeight(2);
+      p.line(x, y, x+size, y+size);
+      p.line(x+size, y, x, y+size);
+  }
+
+  p.close_button_border_thickness = 0;
+  p.closebuttonAlpha = 0;
+  p.closebuttonToggle = -1;
+  p.modal_alpha = 0;
+  p.easing = 0.15;
+  p.modal_w = 20;
+  p.modal_h = 20;
+  p.togglebool = false;
+  p.bgalpha = 0;
+  p.hover_bool = false;
+
+  p.buttonRegion_x = window.innerWidth/2+134;
+  p.buttonRegion_y = window.innerHeight/2-77;
+  p.modalRegion_x = window.innerWidth/2-175;
+  p.modalRegion_y = window.innerHeight/2-90;
+  p.button_color = p.color(182,80,44,0);
+  
+  p.mouseClicked = function(){  
+    if((p.buttonRegion_x < p.mouseX && p.buttonRegion_x+30 > p.mouseX)
+      && (p.buttonRegion_y < p.mouseY && p.buttonRegion_y+30 > p.mouseY)
+      || p.modalRegion_x > p.mouseX || p.modalRegion_x + 350 < p.mouseX
+      || p.modalRegion_y > p.mouseY || p.modalRegion_y + 220 < p.mouseY)
+      {
+        modal_state = false;
+        p.close_button_border_thickness = 0;
+      }
+  }
+
+  p.button_hover = function(){
+    if((p.buttonRegion_x < p.mouseX && p.buttonRegion_x+30 > p.mouseX)
+    && (p.buttonRegion_y < p.mouseY && p.buttonRegion_y+30 > p.mouseY) && modal_state == true)
+    {
+      p.button_color = p.lerpColor(p.button_color,p.color(232,130,104,p.closebuttonAlpha),0.23);
+      p.hover_bool = true;
+    }
+    else
+    {
+      p.button_color = p.lerpColor(p.button_color,p.color(182,80,44,p.closebuttonAlpha),0.23);
+      p.hover_bool = false;
+    }
+  }
+  
+  window.onresize = function() {
+    // assigns new values for width and height variables
+    p.w = window.innerWidth;
+    p.h = window.innerHeight;  
+    p.cnv.size(p.w,p.h);
+  }
 }
 
-
-let superP5_3 = new p5(sound_sketch);
-let superP5_2 = new p5(ink_sketch); // 스케치의 레이어를 결정함, 선입선출
 let superP5_1 = new p5(test_sketch);
+let superP5_2 = new p5(ink_sketch); // 스케치의 레이어를 결정함, 선입선출
+let superP5_3 = new p5(sound_sketch_N_blockingGUI);
