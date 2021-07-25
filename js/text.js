53e518695557cc6096d3b46c9eb68b3e3c2cb8a8
sanhead <69696645+parthuarnax@users.noreply.github.com>
2021-07-26 04:34:27 +0900

    1. scroll guide fix 2. made loading animation faster

--- js/text.js
+++ js/text.js
@@ -16,7 +16,10 @@ function inputButton(){
 		document.getElementById('wt_3').style.visibility = 'visible';
 		document.getElementById('wt_4').style.visibility = 'visible';
 		document.getElementById('B1_P').style.visibility = 'visible';
-		document.getElementById('wt_scroll_guide').style.visibility = 'visible';
+		let guideClass = document.getElementsByClassName('wt_scroll_guide');
+		for(let i = 0; i < guideClass.length; i++){
+			document.getElementsByClassName('wt_scroll_guide')[i].style.visibility = 'visible';
+		}
 		currentWishText = document.getElementById('wishtext').value;
 	}
 	let d = new Date();
